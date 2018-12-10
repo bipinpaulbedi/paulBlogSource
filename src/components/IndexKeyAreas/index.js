6405b70efd8e710fe9bf4fcee416444032fea062
BIPIN PAUL BEDI <bipinpaulbedi@gmail.com>
2018-12-11 10:06:45 +1100

    Added key category component

---
+++ src/components/IndexKeyAreas/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class IndexBanner extends React.PureComponent {
+    render() {
+        return(<React.Fragment>
+        <div>
+        <hr />
+        </div>
+        </React.Fragment>)
+    }
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from '../components/Header'
 import IndexBanner from '../components/IndexBanner'
+import IndexKeyAreas from '../components/IndexKeyAreas'
 
 export default class Home extends React.PureComponent {
     render() {
@@ -8,6 +9,8 @@ export default class Home extends React.PureComponent {
             <React.Fragment>
         <Header />
         <IndexBanner />
+        <hr/>
+        <IndexKeyAreas />
         </React.Fragment>
         )
     }
