909f350239982b28640b53bad8b4d85107915c58
Bipin Bedi <bipin.bedi@charterhall.com.au>
2019-01-01 22:36:45 +1100

    updated theme import

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,6 +1,5 @@
 import Typography from 'typography'
-import theme from '@bipinpaulbedi/paul-blog-typography-theme'
-
+import { theme } from '@bipinpaulbedi/paul-blog-typography-theme'
 const typography = new Typography(theme)
 
 // Hot reload typography in development.
